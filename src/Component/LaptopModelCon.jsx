45616bb1ac9fa9ad1e6704901f593d9f4c994eef
omkarGudappe <omkar.gudappe205@gmail.com>
2025-08-11 11:11:26 +0530

    Models Commentout

--- src/Component/LaptopModelCon.jsx
+++ src/Component/LaptopModelCon.jsx
@@ -1,7 +1,7 @@
 import React , { useRef } from 'react'
 import gsap from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
-import LapTopModel from '../Model/LapTopModel'
+// import LapTopModel from '../Model/LapTopModel'
 import { Suspense } from 'react'
 // import LavaPlanet from '../3DModals/LavaPlanet'
 import BackGround from './BackGround'
@@ -20,9 +20,9 @@ const LaptopModelCon = () => {
       <div className='absolute top-10 left-10 z-2 font-bold text-3xl text-purple-800'>
         <h1>Hi, I'm Omkar</h1>
       </div>
-      <div id='LapTopModelContainer' className="flex justify-center items-center absolute -left-25 lg:left-120 top-40 z-20">
+      {/* <div id='LapTopModelContainer' className="flex justify-center items-center absolute -left-25 lg:left-120 top-40 z-20">
         <LapTopModel mainRef={mainRef} />
-      </div>
+      </div> */}
        {/* <div  className='select-none absolute top-0 left-100 w-full h-1/2 flex justify-center items-center'>
          <LavaPlanet/>
         </div> */}
